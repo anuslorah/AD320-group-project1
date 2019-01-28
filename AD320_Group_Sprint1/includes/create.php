ece1006ef95ad0deec62d526c7b4750727e080c0
Miguel Acevedo <migtomace@gmail.com>
2019-01-28 01:49:56 -0800

    This commits Create php

--- AD320_Group_Sprint1/includes/create.php
+++ AD320_Group_Sprint1/includes/create.php
@@ -9,44 +9,31 @@
 
 if (isset($_POST['submit'])){
 
-    include 'dbh.php';
+        include 'dbh.php';
 
-    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
-    $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $street = mysqli_real_escape_string($conn, $_POST['street']);
+        $city = mysqli_real_escape_string($conn, $_POST['city']);
+        $zip = mysqli_real_escape_string($conn, $_POST['zip']);
+        $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+        $hh = mysqli_real_escape_string($conn, $_POST['hh']);
+        $awesome = mysqli_real_escape_string($conn, $_POST['awesome']);
 
-    //error handlers
-    //check if inputs are empty
-    if (empty($uid) || empty($pwd)){
-        header("Location: ../index.php?login=empty");
-        exit();
-    } else {
-        $sql = "SELECT * FROM users WHERE user_uid='$uid'";
-        $result = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($result);
-        if ($resultCheck < 1){
-            header("Location: ../index.php?login=errorResult:$resultCheck");
+        //error handlers
+        //check if inputs are empty
+        if (empty($name) || empty($street) || empty($city) || empty($zip) || empty($phone) || empty($hh) || empty($awesome)){
+            header("Location: ../index.php?field=empty");
             exit();
         } else {
-            if ($row = mysqli_fetch_assoc($result)){
-                //de-hashing the password
-                $hashedPwdCheck = password_verify($pwd, $row['user_pwd']);
-                if($hashedPwdCheck == false){
-                    header("Location: ../index.php?login=errorPwd");
-                    exit();
-                } elseif ($hashedPwdCheck == true) {
-                    //log in the user here
-                    $_SESSION['u_id'] = $row['user_id'];
-                    $_SESSION['u_first'] = $row['user_first'];
-                    $_SESSION['u_last'] = $row['user_last'];
-                    $_SESSION['u_email'] = $row['user_email'];
-                    $_SESSION['u_uid'] = $row['user_uid'];
-                    header("Location: ../index.php?login=success");
-                    exit();
-                }
+
+            $sql = "INSERT INTO `bar` (`barName`, `streetAddress`, `city`, `zipcode`, `phone`, `happyHour`, `awesome`) VALUES ('$name', '$street', '$city', '$zip', '$phone', '$hh', '$awesome')";
+            if (mysqli_query($conn, $sql)) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
         }
+    }else {
+        header("Location: ../index.php?create=error");
+        exit();
     }
-}else {
-    header("Location: ../index.php?login=errorElse");
-    exit();
-}
