61e7d14e1fb2393103b9cc786b0d4f02eb71da50
Anu Slorah <anuslorah@hotmail.com>
2019-01-29 15:16:40 -0800

    Add files via upload
    links up the form for create

--- AD320_Group_Sprint1/page1.php
+++ AD320_Group_Sprint1/page1.php
@@ -3,12 +3,9 @@
 
 
 <div id="formdiv">
-<?php include 'includes/form.php'; ?>
+<?php include 'includes/createform.php'; ?>
 </div>
  
- <br>
-
- <button type="submit" form="form1" value="Submit">Submit</button> 
 
 </div> <!--this is not error/opening div is in header.php-->
 <?php include 'includes/footer.php'; ?>
