664fc199a094204e2fe8ea81c7c965c504d087b7
Robel <robezal@gmail.com>
2019-02-12 21:34:06 -0800

    Update createform.php

--- AD320-group-project1-master/AD320_Group_Sprint1/includes/createform.php
+++ AD320-group-project1-master/AD320_Group_Sprint1/includes/createform.php
@@ -1,19 +1,53 @@
-<form action="includes/create.php" method="POST">
-	<h3>Add a bar:</h3>
-	<label>Bar name:</label><input type="text" name="name" tabindex="10" size="30">
-	<br>
-	<label>Street Address:</label>
-	<input type="text" name="streetAddress" tabindex="10" size="30">
-	<br>
-	<label>City:</label><input type="text" name="city" tabindex="10" size="30">
-	<br>
-	<label>Zip code:</label><input type="text" name="zip">
-	<br>
-	<label>Phone number:</label><input type="text" name="phone" tabindex="10" size="30">
-	<br>
-	<label>Time of happy hour:</label><input type="text" name="happyhour" cols="36" rows="4" tabindex="40">
-	<br>
-	<label>Why is it awesome?</label><input type="text" name="awesome" cols="36" rows="4" tabindex="40">
-	<br>
-	<label></label><input type="submit">
+<form action="includes/create.php" method="POST" onsubmit="return val();" name="form">
+<div class='grid-container'>
+    <div class='grid-item'>
+        <label for="bar name">Bar name:</label>
+    </div>
+    <div class='grid-item2'>
+        <input type="text" name="name" tabindex="10" size="30">
+    </div>
+    <div class='grid-item'>
+        <label for="street address">Street Address:</label>
+    </div>
+    <div class='grid-item2'>
+        <input type="text" name="street" tabindex="10" size="30">
+    </div>
+    <div class='grid-item'>
+        <label for="city">City:</label>
+    </div>
+    <div class='grid-item2'>
+        <input type="text" name="city" tabindex="10" size="30">
+    </div>
+    <div class='grid-item'>
+        <label for="zipcode">Zip code:</label>
+    </div>
+    <div class='grid-item2'>
+        <input type="text" name="zip" tabindex="10" size="30">
+    </div>
+    <div class='grid-item'>
+        <label for="phone number">Phone number:</label>
+    </div>
+    <div class='grid-item2'>
+        <input type="text" name="phone" tabindex="10" size="30">
+    </div>
+    <div class='grid-item'>
+        <label for="happy hour">Time of happy hour:</label>
+    </div>
+    <div class='grid-item2'>
+        <input type="text" name="hh"  tabindex="10" size="30">
+    </div>
+    <div class='grid-item'>
+        <label for="awesome">Why is it awesome?</label>
+    </div>
+    <div class='grid-item2'>
+        <textarea type="text" name="awesome" cols="30" rows="4" tabindex="30"></textarea>
+    </div>
+    <div class='grid-item'>
+        <label for="submit"></label>
+    </div>
+    <div class='grid-item2'>
+        <input class="submit" type="submit" name="submit" value="create">
+    </div>
+    <div id="req"></div>
+</div>
 </form>
