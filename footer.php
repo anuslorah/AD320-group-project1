cb0c98fa9493f7eb36c72f29deda579879436fe3
han77x <32688245+han77x@users.noreply.github.com>
2019-02-12 17:28:37 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- START Footer -->
+<footer>
+    <p class="footerfont"><small>&copy; <?=date('Y')?> by <a href="https://github.com/anuslorah/AD320-group-project1" target="_blank">GroupWebDevTeamX1</a>, All Rights Reserved ~ <a href="index.php"><i class="fa fa-fw fa-home"></i>HOME</a>
+    
+    <a href="https://www.facebook.com/"target="_blank"><img src="images/fb.png" alt="facebook" class="fb"></a>
+    <a href="https://twitter.com/"target="_blank"><img src="images/twitter.png" alt="twitter" class="twitter"></a></small></p> 
+
+	
+	
+	
+</footer>
+<!-- END Footer --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
